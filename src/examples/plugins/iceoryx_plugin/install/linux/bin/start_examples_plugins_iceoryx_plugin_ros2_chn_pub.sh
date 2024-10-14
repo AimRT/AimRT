69ccda4268006d07df05f481b857cbb68f74c46b
wtudio <95963900+wtudio@users.noreply.github.com>
2024-10-14 18:48:07 +0800

    fix iceoryx plugin example (#25)

--- src/examples/plugins/iceoryx_plugin/install/linux/bin/start_examples_plugins_iceoryx_plugin_ros2_chn_pub.sh
+++ src/examples/plugins/iceoryx_plugin/install/linux/bin/start_examples_plugins_iceoryx_plugin_ros2_chn_pub.sh
@@ -1,5 +1,6 @@
 
 #!/bin/bash
-source install/share/ros2_plugin_proto/local_setup.bash
+
+source install/share/example_ros2/local_setup.bash
 
 ./aimrt_main --cfg_file_path=./cfg/examples_plugins_iceoryx_plugin_ros2_chn_pub_cfg.yaml
