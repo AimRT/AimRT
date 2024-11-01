ae055e96a538081f7494383b859dd1866c1eaa78
zhangyi <zhangyi@agibot.com>
2024-11-01 16:10:19 +0800

    feat: improve timer functionality and logging
    Enhance the timer with more precise intervals and additional logging for better tracking of execution and reset events. Adjust execution periods for improved timing accuracy and introduce dynamic start behavior for better flow control.

--- src/examples/cpp/executor/module/timer_module/timer_module.cc
+++ src/examples/cpp/executor/module/timer_module/timer_module.cc
@@ -26,26 +26,39 @@ bool TimerModule::Start() {
     auto now = std::chrono::system_clock::now();
 
     static int count = 0;
-    static auto last_time = now;
+    static auto start_time = now;
 
     count += 1;
-    auto interval = std::chrono::duration_cast<std::chrono::microseconds>(now - last_time).count();
-    AIMRT_HL_INFO(logger, "Executed {} times, execute interval: {} us", count, interval);
-
-    last_time = now;
-    std::this_thread::sleep_for(300ms);
+    auto interval = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
+    AIMRT_HL_INFO(logger, "Executed {} times, execute time: {} ms", count, interval);
   };
 
-  timer_ = aimrt::executor::CreateTimer(timer_executor_, 1s, std::move(task));
+  timer_ = aimrt::executor::CreateTimer(timer_executor_, 500ms, std::move(task));
+  AIMRT_INFO("Timer created with auto start with 500 ms period");
 
-  timer_executor_.ExecuteAfter(1500ms, [this, logger = core_.GetLogger()]() {
+  timer_executor_.ExecuteAfter(1800ms, [this, logger = core_.GetLogger()]() {
     timer_->Reset();
-    AIMRT_HL_INFO(logger, "Timer reset");
+    AIMRT_HL_INFO(logger, "Timer reset at 1800 ms");
   });
 
   timer_executor_.ExecuteAfter(5000ms, [this, logger = core_.GetLogger()]() {
     timer_->Cancel();
-    AIMRT_HL_INFO(logger, "Timer cancelled");
+    AIMRT_HL_INFO(logger, "Timer cancelled at 5000 ms");
+  });
+
+  timer_executor_.ExecuteAfter(5500ms, [this, logger = core_.GetLogger()]() {
+    timer_->Start(300ms);
+    AIMRT_HL_INFO(logger, "Timer start with 300 ms period at 5500 ms");
+  });
+
+  timer_executor_.ExecuteAfter(6000ms, [this, logger = core_.GetLogger()]() {
+    timer_->Start();
+    AIMRT_HL_INFO(logger, "Timer start with previous 300 ms period at 6000 ms");
+  });
+
+  timer_executor_.ExecuteAfter(8000ms, [this, logger = core_.GetLogger()]() {
+    timer_->Cancel();
+    AIMRT_HL_INFO(logger, "Timer cancelled at 8000 ms");
   });
 
   return true;
