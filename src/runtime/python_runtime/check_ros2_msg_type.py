219a3002f7cbb591a1ac8be6db39c22c527d602d
zhangyi <zhangyi@agibot.com>
2024-11-11 16:06:48 +0800

    fix: remove unnecessary error message for ROS 1 type detection
    Eliminate confusion by removing the printed warning about ROS 1 message types, as it was not providing significant value in handling unsupported types.

--- src/runtime/python_runtime/check_ros2_msg_type.py
+++ src/runtime/python_runtime/check_ros2_msg_type.py
@@ -8,11 +8,6 @@ def check_for_ros2_type_support(msg_or_srv_type):
     try:
         ts = msg_or_srv_type.__class__._TYPE_SUPPORT
     except AttributeError:
-        print(
-            "This might be a ROS 1 message type but it should be a ROS 2 message type. "
-            "Make sure to source your ROS 2 workspace after your ROS 1 workspace.",
-            file=sys.stderr,
-        )
         return False
     if ts is None:
         msg_or_srv_type.__class__.__import_type_support__()
