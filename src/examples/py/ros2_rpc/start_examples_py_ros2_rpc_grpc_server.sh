475b6af343722c6ac9dc846d8d549339dbd20e4e
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2024-11-14 16:23:11 +0800

    chore: update configuration file paths for gRPC client and server examples
    Align the configuration file paths in the example scripts to use the gRPC-specific settings, ensuring the correct configuration is utilized for both the client and server.

--- src/examples/py/ros2_rpc/start_examples_py_ros2_rpc_grpc_server.sh
+++ src/examples/py/ros2_rpc/start_examples_py_ros2_rpc_grpc_server.sh
@@ -3,4 +3,4 @@
 export AIMRT_PLUGIN_DIR=$(pip show aimrt_py | grep Location | awk '{print $2}')/aimrt_py
 source $AIMRT_PLUGIN_DIR/share/example_ros2/local_setup.bash
 
-python3 ./examples_py_ros2_rpc_server_app.py --cfg_file_path=./cfg/examples_py_ros2_rpc_ros2_server_cfg.yaml
+python3 ./examples_py_ros2_rpc_server_app.py --cfg_file_path=./cfg/examples_py_ros2_rpc_grpc_server_cfg.yaml
