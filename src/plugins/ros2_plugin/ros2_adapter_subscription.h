fe300e0a9444348b8afd6572e751c3791a865841
Corsair-cxs <1494089474@qq.com>
2025-01-03 06:22:31 +0800

    fix: ros2 iron build error fix

--- src/plugins/ros2_plugin/ros2_adapter_subscription.h
+++ src/plugins/ros2_plugin/ros2_adapter_subscription.h
@@ -18,9 +18,17 @@ class Ros2AdapterSubscription : public rclcpp::SubscriptionBase {
       const rosidl_message_type_support_t& type_support_handle,
       const std::string& topic_name,
       const rcl_subscription_options_t& subscription_options,
-      const runtime::core::channel::SubscribeWrapper& subscribe_wrapper,
+      const aimrt::runtime::core::channel::SubscribeWrapper& subscribe_wrapper,
       const aimrt::runtime::core::channel::SubscribeTool& sub_tool)
-      : rclcpp::SubscriptionBase(node_base, type_support_handle, topic_name, subscription_options),
+      : rclcpp::SubscriptionBase(
+        node_base, 
+        type_support_handle, 
+        topic_name, 
+        subscription_options,
+        rclcpp::SubscriptionEventCallbacks{},  // 空的事件回调
+        false,                                // 不使用 intra-process
+        rclcpp::DeliveredMessageKind::ROS_MESSAGE  // 默认消息传递类型
+      ),
         subscribe_wrapper_(subscribe_wrapper),
         sub_tool_(sub_tool) {}
 
@@ -46,6 +54,24 @@ class Ros2AdapterSubscription : public rclcpp::SubscriptionBase {
   void return_serialized_message(
       std::shared_ptr<rclcpp::SerializedMessage>& message) override;
 
+  rclcpp::dynamic_typesupport::DynamicMessageType::SharedPtr
+      get_shared_dynamic_message_type() override;
+  
+  rclcpp::dynamic_typesupport::DynamicMessage::SharedPtr
+      get_shared_dynamic_message() override;
+
+  rclcpp::dynamic_typesupport::DynamicSerializationSupport::SharedPtr
+      get_shared_dynamic_serialization_support() override;
+  
+  rclcpp::dynamic_typesupport::DynamicMessage::SharedPtr
+      create_dynamic_message() override;
+
+  void return_dynamic_message(rclcpp::dynamic_typesupport::DynamicMessage::SharedPtr & message) override;
+
+  void handle_dynamic_message(
+    const rclcpp::dynamic_typesupport::DynamicMessage::SharedPtr & message,
+    const rclcpp::MessageInfo & message_info) override;
+  
   void Start() { run_flag_.store(true); }
   void Shutdown() { run_flag_.store(false); }
 
