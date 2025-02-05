40da01eb734f797bcf72c8e08c999c2bf9056043
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2025-02-05 17:06:23 +0800

    feat: add aggregate header generation for interface directories
    This commit introduces a new script `generate_aggregate_headers.py` to automatically generate aggregate header files for various interface directories. The script:
    
    - Walks through specified interface directories
    - Collects all .h header files
    - Creates an aggregate header file that includes all headers
    - Supports multiple interface directories like core plugin, module C/C++/protobuf/ROS2, and package interfaces

---
+++ src/interface/aimrt_module_protobuf_interface/aimrt_module_protobuf_interface.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 The AimRT Authors.
+// AimRT is licensed under Mulan PSL v2.
+
+#pragma once
+
+#include "aimrt_module_protobuf_interface/channel/protobuf_channel.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_tools.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_type_support.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_zero_copy_stream.h"
---
+++ src/interface/aimrt_type_support_pkg_c_interface/aimrt_type_support_pkg_c_interface.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 The AimRT Authors.
+// AimRT is licensed under Mulan PSL v2.
+
+#pragma once
+
+#include "aimrt_type_support_pkg_c_interface/type_support_pkg_export.h"
+#include "aimrt_type_support_pkg_c_interface/type_support_pkg_main.h"
