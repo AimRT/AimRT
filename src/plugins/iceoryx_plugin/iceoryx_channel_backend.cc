77f47ca2b8cafcd87e7393d59c6c45260566257c
hanjun <hanjun@agibot.com>
2025-02-06 10:40:05 +0800

    remove iceoryx plugin iox_write_mutex

--- src/plugins/iceoryx_plugin/iceoryx_channel_backend.cc
+++ src/plugins/iceoryx_plugin/iceoryx_channel_backend.cc
@@ -221,90 +221,86 @@ void IceoryxChannelBackend::Publish(runtime::core::channel::MsgWrapper& msg_wrap
     bool is_shm_enough = true;
     uint64_t msg_size = 0;
 
-    {
-      // avoid muti-thread write the same shm casuing write-fault
-      std::lock_guard<std::mutex> lock(iox_write_mutex_);
-      do {
-        uint64_t loan_size = iox_pub_shm_size_map_[iceoryx_pub_topic];
+    do {
+      uint64_t loan_size = iox_pub_shm_size_map_[iceoryx_pub_topic];
 
-        // release old shm
-        if (iox_pub_loaned_shm_ptr != NULL) {
-          iox_pub->release(iox_pub_loaned_shm_ptr);
-        }
-
-        // load a new size shm
-        auto loan_result = iox_pub->loan(loan_size);
-        iox_pub_loaned_shm_ptr = loan_result.value();
-
-        // write info pkg on loaned shm : the first FIXED_LEN bytes needs to write the length of pkg
-        util::BufferOperator buf_oper(reinterpret_cast<char*>(iox_pub_loaned_shm_ptr) + 4, loan_size - 4);
-
-        // write serialization type on loaned shm
-        buf_oper.SetString(serialization_type, util::BufferLenType::kUInt8);
-
-        // write context meta on loaned shm
-        buf_oper.SetUint8(static_cast<uint8_t>(keys.size()));
-        for (const auto& s : context_meta_kv) {
-          buf_oper.SetString(s, util::BufferLenType::kUInt16);
-        }
-
-        auto type_and_ctx_len = 1 + serialization_type.size() + context_meta_kv_size;
-
-        // write msg on loaned shm： should start at the (FIXED_LEN + type_and_ctx_len)-th byte
-        aimrt::util::IceoryxBufferArrayAllocator iox_allocator(buf_oper.GetRemainingSize(), static_cast<char*>(iox_pub_loaned_shm_ptr) + type_and_ctx_len + 4);
-        if (buffer_array_cache_ptr == nullptr) {
-          try {
-            auto result = SerializeMsgSupportedIceoryx(msg_wrapper, serialization_type, aimrt::util::BufferArrayAllocatorRef(iox_allocator.NativeHandle()));
-            msg_size = result.second;
-            buffer_array_cache_ptr = result.first;
-
-            if (buffer_array_cache_ptr == nullptr) {
-              // in this case means no cache is set, then do nomal serialization(if size is small will throw exception)
-              is_shm_enough = true;
-            } else {
-              if (msg_size > buf_oper.GetRemainingSize()) {
-                // in this case means the msg has serialization cache but the size is too large, then expand suitable size
-                is_shm_enough = false;
-                iox_pub_shm_size_map_[iceoryx_pub_topic] = msg_size + type_and_ctx_len + 4;
-
-              } else {
-                // in this case means the msg has serialization cache and the size is suitable, then use cachema
-                is_shm_enough = true;
-              }
-            }
-
-          } catch (const std::exception& e) {
-            if (!iox_allocator.IsShmEnough()) {
-              // the shm is not enough, need to expand a double size
-              iox_pub_shm_size_map_[iceoryx_pub_topic] = iox_pub_shm_size_map_[iceoryx_pub_topic] << 1;
-              is_shm_enough = false;
-            } else {
-              AIMRT_ERROR(
-                  "Msg serialization failed, serialization_type {}, pkg_path: {}, module_name: {}, topic_name: {}, msg_type: {}, exception: {}",
-                  serialization_type, info.pkg_path, info.module_name, info.topic_name, info.msg_type, e.what());
-              return;
-            }
-          }
-        }
-      } while (!is_shm_enough);
-
-      // if has cache, the copy it to shm to replace the serialization
-      if (buffer_array_cache_ptr != nullptr) {
-        char* strat_pos = static_cast<char*>(iox_pub_loaned_shm_ptr) + 4 + context_meta_kv_size + serialization_type.size() + 1;
-        for (size_t ii = 0; ii < buffer_array_cache_ptr->Size(); ++ii) {
-          std::memcpy(strat_pos, buffer_array_cache_ptr.get()[ii].Data()->data, buffer_array_cache_ptr.get()[ii].Data()->len);
-          strat_pos += buffer_array_cache_ptr.get()[ii].Data()->len;
-        }
-
-        buffer_array_cache_ptr = nullptr;
+      // release old shm
+      if (iox_pub_loaned_shm_ptr != NULL) {
+        iox_pub->release(iox_pub_loaned_shm_ptr);
       }
 
-      // write info pkg length on loaned shm
-      uint32_t data_size = 1 + serialization_type.size() + context_meta_kv_size + msg_size;
-      util::SetBufFromUint32(reinterpret_cast<char*>(iox_pub_loaned_shm_ptr), data_size);
+      // load a new size shm
+      auto loan_result = iox_pub->loan(loan_size);
+      iox_pub_loaned_shm_ptr = loan_result.value();
 
-      iox_pub->publish(iox_pub_loaned_shm_ptr);
+      // write info pkg on loaned shm : the first FIXED_LEN bytes needs to write the length of pkg
+      util::BufferOperator buf_oper(reinterpret_cast<char*>(iox_pub_loaned_shm_ptr) + 4, loan_size - 4);
+
+      // write serialization type on loaned shm
+      buf_oper.SetString(serialization_type, util::BufferLenType::kUInt8);
+
+      // write context meta on loaned shm
+      buf_oper.SetUint8(static_cast<uint8_t>(keys.size()));
+      for (const auto& s : context_meta_kv) {
+        buf_oper.SetString(s, util::BufferLenType::kUInt16);
+      }
+
+      auto type_and_ctx_len = 1 + serialization_type.size() + context_meta_kv_size;
+
+      // write msg on loaned shm： should start at the (FIXED_LEN + type_and_ctx_len)-th byte
+      aimrt::util::IceoryxBufferArrayAllocator iox_allocator(buf_oper.GetRemainingSize(), static_cast<char*>(iox_pub_loaned_shm_ptr) + type_and_ctx_len + 4);
+      if (buffer_array_cache_ptr == nullptr) {
+        try {
+          auto result = SerializeMsgSupportedIceoryx(msg_wrapper, serialization_type, aimrt::util::BufferArrayAllocatorRef(iox_allocator.NativeHandle()));
+          msg_size = result.second;
+          buffer_array_cache_ptr = result.first;
+
+          if (buffer_array_cache_ptr == nullptr) {
+            // in this case means no cache is set, then do nomal serialization(if size is small will throw exception)
+            is_shm_enough = true;
+          } else {
+            if (msg_size > buf_oper.GetRemainingSize()) {
+              // in this case means the msg has serialization cache but the size is too large, then expand suitable size
+              is_shm_enough = false;
+              iox_pub_shm_size_map_[iceoryx_pub_topic] = msg_size + type_and_ctx_len + 4;
+
+            } else {
+              // in this case means the msg has serialization cache and the size is suitable, then use cachema
+              is_shm_enough = true;
+            }
+          }
+
+        } catch (const std::exception& e) {
+          if (!iox_allocator.IsShmEnough()) {
+            // the shm is not enough, need to expand a double size
+            iox_pub_shm_size_map_[iceoryx_pub_topic] = iox_pub_shm_size_map_[iceoryx_pub_topic] << 1;
+            is_shm_enough = false;
+          } else {
+            AIMRT_ERROR(
+                "Msg serialization failed, serialization_type {}, pkg_path: {}, module_name: {}, topic_name: {}, msg_type: {}, exception: {}",
+                serialization_type, info.pkg_path, info.module_name, info.topic_name, info.msg_type, e.what());
+            return;
+          }
+        }
+      }
+    } while (!is_shm_enough);
+
+    // if has cache, the copy it to shm to replace the serialization
+    if (buffer_array_cache_ptr != nullptr) {
+      char* strat_pos = static_cast<char*>(iox_pub_loaned_shm_ptr) + 4 + context_meta_kv_size + serialization_type.size() + 1;
+      for (size_t ii = 0; ii < buffer_array_cache_ptr->Size(); ++ii) {
+        std::memcpy(strat_pos, buffer_array_cache_ptr.get()[ii].Data()->data, buffer_array_cache_ptr.get()[ii].Data()->len);
+        strat_pos += buffer_array_cache_ptr.get()[ii].Data()->len;
+      }
+
+      buffer_array_cache_ptr = nullptr;
     }
+
+    // write info pkg length on loaned shm
+    uint32_t data_size = 1 + serialization_type.size() + context_meta_kv_size + msg_size;
+    util::SetBufFromUint32(reinterpret_cast<char*>(iox_pub_loaned_shm_ptr), data_size);
+
+    iox_pub->publish(iox_pub_loaned_shm_ptr);
 
     AIMRT_TRACE("Iceoryx publish to '{}'", iceoryx_pub_topic);
 
