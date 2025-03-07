d8955b2a2647b6189a88f93645059f27545ee6a1
zhangyi1357 <zhangyi_1357@sjtu.edu.cn>
2025-03-07 10:04:06 +0800

    feat: add multi-version documentation support for Sphinx

--- document/sphinx-cn/conf.py
+++ document/sphinx-cn/conf.py
@@ -15,16 +15,34 @@ release = 'v0.10.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'myst_parser',
-    'sphinx_design',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_multiversion",
+    "myst_parser",
+    "sphinx_design",
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# smv_tag_whitelist = r"^v\d+\.\d+\.\d+$"  # 构建指定的标签
+smv_tag_whitelist = r"^(v0\.9\.3|v0\.8\.3)$"
+
+# Whitelist pattern for branches (set to None to ignore all branches)
+smv_branch_whitelist = None
+
+# Whitelist pattern for remotes (set to None to use local branches only)
+smv_remote_whitelist = None
+
+# Pattern for released versions
+smv_released_pattern = r"^.*$"
+
+# Format for versioned output directories inside the build directory
+smv_outputdir_format = "{ref.name}"
+
+# Determines whether remote or local git branches/tags are preferred if their output dirs conflict
+smv_prefer_remote_refs = False
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -34,6 +52,10 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    "display_version": True,
+}
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -64,3 +86,11 @@ html_show_sourcelink = False
 html_css_files = [
     "css/custom.css",
 ]
+
+html_context = {
+    "current_version": "v0.9.3",
+    "versions": [
+        {"name": "v0.9.3", "url": "./v0.9.3/"},
+        {"name": "v0.8.3", "url": "./v0.8.3/"},
+    ],
+}
