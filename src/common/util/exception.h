87a5a18456a8a28ac7116804df0b2fcf26e51dbb
yuguanlin <yuguanlin@agibot.com>
2025-10-31 10:15:44 +0800

    refactor: update AIMRT_ASSERT macro to utilize std::source_location for enhanced debugging context

--- src/common/util/exception.h
+++ src/common/util/exception.h
@@ -27,23 +27,23 @@ class AimRTException : public std::exception {
 
 }  // namespace aimrt::common::util
 
-#define AIMRT_ASSERT(__expr__, __fmt__, ...)                       \
-  do {                                                             \
-    if (!(__expr__)) [[unlikely]] {                                \
-      throw aimrt::common::util::AimRTException(                   \
-          ::aimrt_fmt::format("[{}:{} @{}]: " __fmt__,                   \
-                              __FILE__,                            \
-                              __LINE__,                            \
-                              __FUNCTION__,                        \
-                              ##__VA_ARGS__));                     \
-    }                                                              \
+#define AIMRT_ASSERT(__expr__, __fmt__, ...)                 \
+  do {                                                       \
+    if (!(__expr__)) [[unlikely]] {                          \
+      auto __location__ = std::source_location::current();   \
+      throw aimrt::common::util::AimRTException(             \
+          ::aimrt_fmt::format("[{}:{} @{}]: " __fmt__,       \
+                              __location__.file_name(),      \
+                              __location__.line(),           \
+                              __FUNCTION__, ##__VA_ARGS__)); \
+    }                                                        \
   } while (0)
 
 #define AIMRT_ASSERT_WITH_LOC(__call_loc__, __expr__, __fmt__, ...) \
   do {                                                              \
     if (!(__expr__)) [[unlikely]] {                                 \
       throw aimrt::common::util::AimRTException(                    \
-          ::aimrt_fmt::format("[{}:{}]: " __fmt__,                    \
+          ::aimrt_fmt::format("[{}:{}]: " __fmt__,                  \
                               __call_loc__.file_name(),             \
                               __call_loc__.line(),                  \
                               ##__VA_ARGS__));                      \
