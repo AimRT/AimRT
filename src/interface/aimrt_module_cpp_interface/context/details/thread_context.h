6cfb3a6b62ede8f184b7231f8a05ea6fee399309
yuguanlin <yuguanlin@agibot.com>
2025-10-31 16:12:11 +0800

    refactor: update GetCurrentContext to use AIMRT_ASSERT_WITH_LOC for enhanced error reporting

--- src/interface/aimrt_module_cpp_interface/context/details/thread_context.h
+++ src/interface/aimrt_module_cpp_interface/context/details/thread_context.h
@@ -29,7 +29,7 @@ inline std::shared_ptr<Context> GetCurrentContext(const std::source_location& ca
   const std::shared_ptr ctx_ptr = g_thread_ctx.ctx_ptr.lock();
 
   if (ctx_ptr == nullptr) [[unlikely]]
-    AIMRT_ASSERT(ctx_ptr != nullptr, "Broken context !");
+    AIMRT_ASSERT_WITH_LOC(call_loc, ctx_ptr != nullptr, "Broken context !");
   return ctx_ptr;
 }
 
