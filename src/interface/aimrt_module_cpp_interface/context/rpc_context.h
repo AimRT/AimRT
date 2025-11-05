42dddeddcce33eb41e1f49698717c863a99857eb
yuguanlin <yuguanlin@agibot.com>
2025-11-05 14:57:24 +0800

    refactor: remove commented-out client and server interface descriptions in RpcResource struct for cleaner code

--- src/interface/aimrt_module_cpp_interface/context/rpc_context.h
+++ src/interface/aimrt_module_cpp_interface/context/rpc_context.h
@@ -11,13 +11,8 @@
 namespace aimrt::context {
 
 struct RpcResource {
-  // 擦除了类型的客户端调用接口（Client<Q, P>）
   std::any call_f;
-
-  // 擦除了类型的服务端处理接口（Server<Q, P>）
   std::any serve_f;
-
-  // 服务方法名称，作为 aimrt 内部 std::string_view 的内容 holder
   std::string func_name;
 };
 
