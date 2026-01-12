c980b8b114c79ae3323a4c46af664e0122ab9a01
yuguanlin <yuguanlin@agibot.com>
2026-01-12 11:31:15 +0800

    test

--- cmake/GetEventsExecutor.cmake
+++ cmake/GetEventsExecutor.cmake
@@ -6,44 +6,32 @@ include(FetchContent)
 message(STATUS "Getting irobot_events_executor...")
 
 set(events_executor_GIT_URL
-    "https://github.com/irobot-ros/events-executor.git"
-    CACHE STRING "")
-
-set(events_executor_GIT_TAG
-    "main"
+    "https://github.com/irobot-ros/events-executor/archive/b2ef94e1ecee3aa3369c680343df46035998ddc0.tar.gz"
     CACHE STRING "")
 
 if(events_executor_LOCAL_SOURCE)
   FetchContent_Declare(
     irobot_events_executor
-    SOURCE_DIR ${events_executor_LOCAL_SOURCE} SOURCE_SUBDIR irobot_events_executor
+    SOURCE_DIR ${events_executor_LOCAL_SOURCE}
     OVERRIDE_FIND_PACKAGE)
 else()
   FetchContent_Declare(
     irobot_events_executor
-    GIT_REPOSITORY
-    ${events_executor_GIT_URL}
-    GIT_TAG
-    ${events_executor_GIT_TAG}
-    SOURCE_SUBDIR
-    irobot_events_executor
+    URL ${events_executor_GIT_URL}
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     OVERRIDE_FIND_PACKAGE)
 endif()
 
 function(get_events_executor)
   FetchContent_GetProperties(irobot_events_executor)
   if(NOT irobot_events_executor_POPULATED)
-    # Populate first so irobot_events_executor_SOURCE_DIR is valid.
     FetchContent_Populate(irobot_events_executor)
 
-    set(_events_exec_cpp
-        "${irobot_events_executor_SOURCE_DIR}/irobot_events_executor/src/rclcpp/executors/events_executor/events_executor.cpp")
+    set(_events_exec_cpp "${irobot_events_executor_SOURCE_DIR}/irobot_events_executor/src/rclcpp/executors/events_executor/events_executor.cpp")
     if(EXISTS "${_events_exec_cpp}")
       file(READ "${_events_exec_cpp}" CONTENTS)
-      string(REPLACE
-             "executor_notifier_->add_guard_condition(interrupt_guard_condition_.get());"
-             "executor_notifier_->add_guard_condition(&interrupt_guard_condition_);"
-             CONTENTS "${CONTENTS}")
+      string(REPLACE "executor_notifier_->add_guard_condition(interrupt_guard_condition_.get());" "executor_notifier_->add_guard_condition(&interrupt_guard_condition_);" CONTENTS
+                     "${CONTENTS}")
       file(WRITE "${_events_exec_cpp}" "${CONTENTS}")
     endif()
 
@@ -54,9 +42,29 @@ function(get_events_executor)
         OFF
         CACHE BOOL "" FORCE)
 
-    # Equivalent to FetchContent_MakeAvailable(), but avoids repopulating.
-    add_subdirectory("${irobot_events_executor_SOURCE_DIR}/irobot_events_executor"
-                     "${irobot_events_executor_BINARY_DIR}")
+    add_subdirectory("${irobot_events_executor_SOURCE_DIR}/irobot_events_executor" "${irobot_events_executor_BINARY_DIR}_events_exec")
+
+    set(_mock_config_dir "${CMAKE_CURRENT_BINARY_DIR}/irobot_events_executor_mock")
+    file(MAKE_DIRECTORY "${_mock_config_dir}")
+
+    file(
+      WRITE "${_mock_config_dir}/irobot_events_executorConfig.cmake"
+      "
+      set(irobot_events_executor_FOUND TRUE)
+
+      if(TARGET irobot_events_executor)
+        if(NOT TARGET irobot_events_executor::irobot_events_executor)
+          add_library(irobot_events_executor::irobot_events_executor ALIAS irobot_events_executor)
+        endif()
+      endif()
+    ")
+
+    set(irobot_events_executor_DIR
+        "${_mock_config_dir}"
+        CACHE PATH "Hack for finding irobot_events_executor built in-tree" FORCE)
+
+    add_subdirectory("${irobot_events_executor_SOURCE_DIR}/irobot_lock_free_events_queue" "${irobot_events_executor_BINARY_DIR}_lock_free_queue")
+
   endif()
 endfunction()
 
--- src/plugins/ros2_plugin/ros2_plugin.cc
+++ src/plugins/ros2_plugin/ros2_plugin.cc
@@ -3,11 +3,12 @@
 
 #include "ros2_plugin/ros2_plugin.h"
 #include "core/aimrt_core.h"
+#include "irobot_lock_free_events_queue/lock_free_events_queue.hpp"
 #include "rcl/logging.h"
+#include "rclcpp/executors/events_executor/events_executor.hpp"
 #include "ros2_plugin/global.h"
 #include "ros2_plugin/ros2_channel_backend.h"
 #include "ros2_plugin/ros2_rpc_backend.h"
-#include "rclcpp/executors/events_executor/events_executor.hpp"
 
 namespace YAML {
 template <>
@@ -83,7 +84,8 @@ bool Ros2Plugin::Initialize(runtime::core::AimRTCore* core_ptr) noexcept {
               rclcpp::ExecutorOptions(), options_.executor_thread_num);
     } else if (options_.executor_type == "EventsExecutor") {
       ros2_node_executor_ptr_ =
-          std::make_shared<rclcpp::executors::EventsExecutor>();
+          std::make_shared<rclcpp::executors::EventsExecutor>(
+              std::make_unique<LockFreeEventsQueue>());
     } else {
       AIMRT_ERROR_THROW("Invalid ros2 executor_type '{}'.",
                         options_.executor_type);
